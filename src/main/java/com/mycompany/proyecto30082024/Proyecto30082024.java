20e7c69ce96b9cd48a0739a43b08f3dda2ff1728
LeonardoTaipe <U23324746@utp.edu.pe>
2024-08-29 21:54:48 -0500

    Primercommit

---
+++ src/main/java/com/mycompany/proyecto30082024/Proyecto30082024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto30082024;
+
+/**
+ *
+ * @author josem
+ */
+public class Proyecto30082024 {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo xdds");
+    }
+}
