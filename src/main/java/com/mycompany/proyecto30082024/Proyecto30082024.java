33835d2c34b6606a34ef43d00fcb7372dd63556f
LeonardoTaipe <U23324746@utp.edu.pe>
2024-08-29 23:04:10 -0500

    Horas

--- src/main/java/com/mycompany/proyecto30082024/Proyecto30082024.java
+++ src/main/java/com/mycompany/proyecto30082024/Proyecto30082024.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.proyecto30082024;
 
+import java.util.Scanner;
+
 /**
  *
  * @author josem
@@ -11,6 +13,46 @@ package com.mycompany.proyecto30082024;
 public class Proyecto30082024 {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo xdds");
+        String segundosString="";
+        String minutosString="";
+        String horaString="";
+        Scanner lectura = new Scanner (System.in);        
+        System.out.println("Ingrese la hora 0-23: ");
+        int hora = lectura.nextInt();
+        System.out.println("Ingrese los minutos 0-59: ");
+        int minutos = lectura.nextInt();
+        System.out.println("Ingrese los segundos 0-59: ");
+        int segundos = lectura.nextInt();
+        segundosString=segundos+"";
+        minutosString=minutos+"";
+        horaString=hora+"";
+        System.out.println(horaString+":"+minutosString+":"+segundosString);
+        segundos=segundos+1;
+        if(segundos>59 ){
+            minutos=minutos+1;
+            segundos=0;
+        }
+        if(minutos>59 ){
+            hora=hora+1;
+            minutos=0;
+        
+        }
+        if(hora>23) {
+        hora=0;
+       
+        }
+        segundosString=segundos+"";
+        minutosString=minutos+"";
+        horaString=hora+"";
+        if(segundosString.length()==1){
+        segundosString="0"+segundosString;
+        }
+        if(minutosString.length()==1){
+        minutosString="0"+minutosString;
+        }
+        if(horaString.length()==1){
+        horaString="0"+horaString;
+        }
+        System.out.println(horaString+":"+minutosString+":"+segundosString);
     }
 }
